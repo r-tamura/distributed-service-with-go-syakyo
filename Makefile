8b8fc7d65f9dc6f81a6ed89c5686ffff7e2edf30
r-tamura <rtamura30@gmail.com>
2022-10-01 16:07:31 +0900

    P32 ストアの実装 - go CLIのraceオプションはgccを必要としていたためgo test -raceビルドエラーが発生した (Makefile参照)

--- Makefile
+++ Makefile
@@ -5,6 +5,8 @@ compile:
 			--go_opt=paths=source_relative \
 			--proto_path=.
 
+# https://github.com/golang/go/issues/27089#issuecomment-933016414
+# -raceオプションをつけるとgccが要求されビルドエラーになる。--vet=offすると動く
 .PHONY: test
 test:
-	go test -race ./...
+	go test -race --vet=off ./...
---
+++ internal/log/store_test.go
@@ -0,0 +1,101 @@
+package log
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	write = []byte("hello world")
+	width = uint64(len(write)) + lenWidth
+)
+
+func TestStoreAppendRead(t *testing.T) {
+	f, err := os.CreateTemp("", "store_append_read_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+
+	testAppend(t, s)
+	testRead(t, s)
+	testReadAt(t, s)
+
+	s, err = newStore(f)
+	require.NoError(t, err)
+	testRead(t, s)
+}
+
+func testAppend(t *testing.T, s *store) {
+	t.Helper()
+	for i := uint64(1); i < 4; i++ {
+		n, pos, err := s.Append(write)
+		require.NoError(t, err)
+		fmt.Println(n, pos, width, i)
+		require.Equal(t, pos+n, width*i)
+	}
+}
+
+func testRead(t *testing.T, s *store) {
+	t.Helper()
+	var pos uint64
+	for i := uint64(1); i < 4; i++ {
+		read, err := s.Read(pos)
+		require.NoError(t, err)
+		require.Equal(t, write, read)
+		pos += width
+	}
+}
+
+func testReadAt(t *testing.T, s *store) {
+	t.Helper()
+	for i, off := uint64(1), int64(0); i < 4; i++ {
+		b := make([]byte, lenWidth)
+		n, err := s.ReadAt(b, off)
+		require.NoError(t, err)
+		require.Equal(t, n, lenWidth)
+		off += int64(n)
+
+		size := enc.Uint64(b)
+		b = make([]byte, size)
+		n, err = s.ReadAt(b, off)
+		require.NoError(t, err)
+		require.Equal(t, n, int(size))
+		off += int64(n)
+	}
+}
+
+func testStoreClose(t *testing.T) {
+	f, err := os.CreateTemp("", "store_close_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+	s, err := newStore(f)
+	require.NoError(t, err)
+	_, _, err = s.Append(write)
+	require.NoError(t, err)
+
+	f, beforeSize, err := openFile(f.Name())
+	require.NoError(t, err)
+
+	err = s.Close()
+	require.NoError(t, err)
+	_, afterSize, err := openFile(f.Name())
+	require.NoError(t, err)
+	require.True(t, afterSize > beforeSize)
+}
+
+func openFile(name string) (file *os.File, size uint64, err error) {
+	f, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
+	if err != nil {
+		return nil, 0, err
+	}
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, 0, err
+	}
+	return f, uint64(fi.Size()), nil
+}
